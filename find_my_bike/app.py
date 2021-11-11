09904824f5645786aad232a1b73c2cdf64bb2565
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-11-11 14:39:33 +0100

    use carroussel for gradio output

--- find_my_bike/app.py
+++ find_my_bike/app.py
@@ -15,16 +15,15 @@ query = DocumentArray(from_files("data/query/*.png"))
 class PrintResult:
     def __init__(self):
         self.__name__ = "PrintResult"
+        self.imgs = []
 
     def __call__(self, resp: Response):
 
-        for doc in resp.docs:
+        doc = resp.docs[0]
 
-            in_memory_file = BytesIO()
-            doc.matches.plot_image_sprites(in_memory_file)
-
-            self.img = Image.open(in_memory_file)
-            return None
+        for match in doc.matches:
+            match.convert_uri_to_image_blob()
+            self.imgs.append(match.blob)
 
 
 def create_query_from_img(img: np.array) -> DocumentArray:
@@ -47,11 +46,11 @@ def search(img: np.array):
         show_progress=True,
     )
 
-    return print_result.img
+    return print_result.imgs
 
 
 inputs = gr.inputs.Image()
 
-outputs = gr.outputs.Image()
+outputs = gr.outputs.Carousel(["image"])
 
 gr.Interface(fn=search, inputs=inputs, outputs=outputs).launch()
